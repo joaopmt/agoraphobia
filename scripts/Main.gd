c0befd56215c1e490943174d3ced98ab1cd8c214
andrezao <andresoranzzo@gmail.com>
2019-01-27 01:11:00 -0200

    changes

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,6 +1,9 @@
 extends Node
 
 var main_room = preload("res://scenes/main_room.tscn")
+var room_2 = preload("res://scenes/room_2.tscn")
+var room_3 = preload("res://scenes/room_3.tscn")
+var building = preload("res://scenes/building.tscn")
 
 onready var rooms = get_node("Rooms")
 onready var player = get_node("Player")
@@ -23,8 +26,14 @@ func inicia():
 	rooms.add_child(inicio)
 	
 func load_room():
+	var new_room
 	if room_key == 0:
 		room_key = 1
-		inicia()
+		#carrega room predio
+		new_room = building.instance()
+		rooms.add_child(new_room)
 		print(rooms.get_children()[0])
+		player.apply_scale(Vector2(0.4, 0.36)
 		player.position = Vector2(278.791748, 449.57843)
+	elif room_key == 1:
+		room_key = 2
